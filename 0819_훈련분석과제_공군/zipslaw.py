1ad105224e4a89a5263437038939d8c038f8da25
Guest <guest-2lljbm@h25709.sgb.skir.kr>
2020-09-10 20:26:56 +0900

    출현빈도 power law 완료
    Signed-off-by: Guest <guest-2lljbm@h25709.sgb.skir.kr>

--- 0819_훈련분석과제_공군/zipslaw.py
+++ 0819_훈련분석과제_공군/zipslaw.py
@@ -2,10 +2,12 @@ from kiwipiepy import Kiwi
 import pickle
 import sqlite3
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
 #prepare tokenizer
 kiwi = Kiwi()
 kiwi.prepare()
-
 
 
 #import stopwords
@@ -35,12 +37,41 @@ con = sqlite3.connect("./training.db")
 query = """
 SELECT *
 FROM
-    naver_news
+    naver_comment
 """
 
 df_naver_news = pd.read_sql(query, con)
 news = df_naver_news.content
 word_count = scan_vocabulary(news, tokenize)
-print(type(word_count))
 
-print(word_count)
+word_sorted = sorted(word_count, key = lambda k: word_count[k], reverse = True)
+count_sorted = [word_count[k] for k in word_sorted]
+
+fig = plt.figure()
+ax1 = fig.add_subplot(121)
+ax2 = fig.add_subplot(122)
+
+log_y = np.log(np.array(count_sorted))
+log_x = np.log(np.arange(1, len(count_sorted)+1))
+
+x_intercept = np.ones(len(count_sorted)).reshape(-1,1)
+x_data = log_x.reshape(-1,1)
+X = np.concatenate((x_intercept, x_data), axis = 1)
+XtX = np.matmul(X.T, X)
+XtX_inv = np.linalg.inv(XtX)
+theta = np.matmul(XtX_inv, np.matmul(X.T, log_y))
+a = np.exp(theta[0])
+k = theta[1]
+
+print('a = {}, k = {}'.format(a, k))
+print(word_sorted[:50])
+print(len(word_sorted))
+ax1.plot(log_x, log_y)
+grid_x = np.linspace(1, log_x[-1], 1000)
+ax1.plot(grid_x, theta[0] + theta[1]  * grid_x )
+
+ax2.plot(count_sorted)
+
+plt.show()
+
+
